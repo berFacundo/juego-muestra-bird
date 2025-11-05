0396cfcd3080257abce25289cf0d51aa253502f7
Facundo <facuberazategui77@gmail.com>
2025-11-05 12:54:52 -0300

    Fuente corregida logo y gamover modificado

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ let maxPredictions;
 
 // Función inicial para cargar el modelo
 async function initModel() {
-  // URL del modelo y metadata
-  const modelURL = MODEL_URL + "model.json";
-  const metadataURL = MODEL_URL + "metadata.json";
+    // URL del modelo y metadata
+    const modelURL = MODEL_URL + "model.json";
+    const metadataURL = MODEL_URL + "metadata.json";
 
-  try {
-    // Cargar el modelo y la metadata
-    model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
-    console.log("Modelo cargado correctamente:", model);
-    console.log("Número de clases:", maxPredictions);
-  } catch (error) {
-    console.error("Error cargando el modelo:", error);
-  }
+    try {
+        // Cargar el modelo y la metadata
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+        console.log("Modelo cargado correctamente:", model);
+        console.log("Número de clases:", maxPredictions);
+    } catch (error) {
+        console.error("Error cargando el modelo:", error);
+    }
 }
 
 // Llamar a la función para inicializar el modelo
@@ -27,6 +27,7 @@ initModel();
 
 
 //Codigo del juego Flappy Bird
+
 
 //Variables globales del juego
 let boardWidth = 940;           //Ancho del canvas el juego
@@ -39,9 +40,9 @@ document.addEventListener("keydown", handleKeyDown);        //Interaccion con el
 
 let GAME_STATE = {              //Estados del juego
     MENU: "menu",
-    PLAYING: "playing", 
+    PLAYING: "playing",
     GAME_OVER: "gameOver"
-}; 
+};
 let currentState = GAME_STATE.MENU;         //Arranca el juego en el menu
 
 let playButton = {                      //Define el boton para jugar
@@ -56,7 +57,7 @@ let logo = {                       //Define el logo del juego
     y: boardHeight / 4,                 //Coordenada y del logo
     width: 300,                         //Ancho del logo
     height: 100                         //Alto del logo           
-}; 
+};
 
 let flappyBirdTextImg = new Image();                        //Define la imagen del texto del juego
 flappyBirdTextImg.src = "./Sprites/flappyBirdLogo.png";      //Ruta de la imagen del texto del juego
@@ -85,7 +86,7 @@ let pipeIntervalId;                 //Intervalo que genera las tuberias
 
 //Funciones de las tuberias
 function placePipes() {         //funcion que maneja las tuberias
-    createPipes();          
+    createPipes();
 }
 
 function createPipes() {        //funcion que crea las tuberias con gap random
@@ -96,16 +97,16 @@ function createPipes() {        //funcion que crea las tuberias con gap random
     let topPipe = {         //Definicion de la tuberia superior
         x: boardWidth,      //Posicion inicial en x de la tuberia, se asegura de que las tuberias aparezcan a la derecha lejos del canvas
         y: 0,               //Posicion inicial en y de la tuberia, se asegura de que la tuberia superior empiece desde arriba del canvas
-        width: pipeWidth,           
+        width: pipeWidth,
         height: topPipeHeight,
         img: topPipeImg,
         passed: false           //Variable para detectar si el pajaro ya paso por esta tuberia
     };
 
     let bottomPipe = {      //Definicion de la tuberia inferior, casi igual que la anterior
-        x: boardWidth, 
-        y: topPipeHeight + pipeGap, 
-        width: pipeWidth, 
+        x: boardWidth,
+        y: topPipeHeight + pipeGap,
+        width: pipeWidth,
         height: bottomPipeHeight,
         img: bottomPipeImg,
         passed: false
@@ -113,8 +114,19 @@ function createPipes() {        //funcion que crea las tuberias con gap random
     pipeArray.push(topPipe, bottomPipe);    //Agrega las tuberias al array de tuberias
 }
 
+// Cargar la fuente antes de iniciar el juego
+const flappyFont = new FontFace('Flappy', 'url(./fuentes/FlappyBirdy.ttf)');
+
+flappyFont.load().then((loadedFont) => {
+    document.fonts.add(loadedFont);
+    console.log('Fuente Flappy cargada correctamente');
+}).catch((error) => {
+    console.error('Error cargando la fuente Flappy:', error);
+});
+
+
 //Iniciar el juego
-window.onload = function() {                            //Funcion que inicia el juego, define el canvas y carga nuestros assets
+window.onload = function () {                            //Funcion que inicia el juego, define el canvas y carga nuestros assets
     board = document.getElementById("board");           //Se conecta con el que esta definido en el HTML
     board.height = boardHeight;                         //Usamos los valores del canvas ya definidos
     board.width = boardWidth;                           //Usamos los valores del canvas ya definidos
@@ -137,27 +149,27 @@ window.onload = function() {                            //Funcion que inicia el 
 
 function update() {                 //Funcion principal del juego, maneja los estados y renderiza todo
     requestAnimationFrame(update);          //Se llama a si misma para crear un loop
-    context.clearRect(0,0, board.width, board.height); //Limpia el canvas
+    context.clearRect(0, 0, board.width, board.height); //Limpia el canvas
 
-    if(currentState === GAME_STATE.MENU) {          //Condicional, si estamos en el menu, renderiza el menu
-        renderMenu(); 
-    } else if(currentState === GAME_STATE.PLAYING) {    //Condicional, si estamos jugando, renderiza el juego
-        renderGame(); 
-    } else if(currentState === GAME_STATE.GAME_OVER) {  //Condicional, si estamos en gameover, renderiza gameover
-        renderGameOver(); 
+    if (currentState === GAME_STATE.MENU) {          //Condicional, si estamos en el menu, renderiza el menu
+        renderMenu();
+    } else if (currentState === GAME_STATE.PLAYING) {    //Condicional, si estamos jugando, renderiza el juego
+        renderGame();
+    } else if (currentState === GAME_STATE.GAME_OVER) {  //Condicional, si estamos en gameover, renderiza gameover
+        renderGameOver();
     }
 }
 
 function renderMenu() {         //Funcion que renderiza el menu
-    if(backgroundImg.complete) {                                                //Si la imagen de fondo esta cargada, renderizarla
-        context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight); 
+    if (backgroundImg.complete) {                                                //Si la imagen de fondo esta cargada, renderizarla
+        context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
     }
 
-    if(playButtonImg.complete) {                                    //Si la imagen del boton de jugar esta cargada renderizarla                  
-        context.drawImage(playButtonImg, playButton.x, playButton.y, playButton.width, playButton.height); 
+    if (playButtonImg.complete) {                                    //Si la imagen del boton de jugar esta cargada renderizarla                  
+        context.drawImage(playButtonImg, playButton.x, playButton.y, playButton.width, playButton.height);
     }
 
-    if(flappyBirdTextImg.complete) {        //Si la imagen del texto del juego esta cargada
+    if (flappyBirdTextImg.complete) {        //Si la imagen del texto del juego esta cargada
         let scaledWidth = logo.width;           //Configurar ancho escalado
         let scaledHeight = (flappyBirdTextImg.height / flappyBirdTextImg.width) * scaledWidth;  //Configurar alto escalado
         context.drawImage(flappyBirdTextImg, logo.x, logo.y, scaledWidth, scaledHeight);   //Renderizar el texto del juego
@@ -165,7 +177,7 @@ function renderMenu() {         //Funcion que renderiza el menu
 }
 
 function renderGame() {         //Funcion que renderiza el juego, se encarga del mov. del pajaro, logica de tuberias, puntaje.
-    if(backgroundImg.complete) {
+    if (backgroundImg.complete) {
         context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);        //Renderiza el fondo
     }
     velocityY += gravity;                 //Aplica gravedad a la velocidad vertical del pajaro
@@ -173,60 +185,64 @@ function renderGame() {         //Funcion que renderiza el juego, se encarga del
 
     // Animacion de rotacion del pajaro
     context.save();  // Guardar el estado del canvas
-    
+
     // Mover el origen al centro del pájaro
-    context.translate(bird.x + bird.width/2, bird.y + bird.height/2);
-    
+    context.translate(bird.x + bird.width / 2, bird.y + bird.height / 2);
+
     // Calcular rotación basada en la velocidad vertical
     let rotation = Math.max(Math.min(velocityY * 2.5, 90), -25) * Math.PI / 180;
     context.rotate(rotation);
 
-    context.drawImage(birdImg, -bird.width/2, -bird.height/2, bird.width, bird.height);    //Renderiza el pajaro
+    context.drawImage(birdImg, -bird.width / 2, -bird.height / 2, bird.width, bird.height);    //Renderiza el pajaro
 
     context.restore();  // Restaurar el estado del canvas
 
     //Condicional para el gameover
-    if(bird.y > board.height) {                 //Condicional, si el pajaro se sale por abajo del canvas
+    if (bird.y > board.height) {                 //Condicional, si el pajaro se sale por abajo del canvas
         currentState = GAME_STATE.GAME_OVER;    //activa el estado game over
     }
 
     //Bucle para manejar todas las tuberias en el array, renderizarlas y sus colisiones
-    for(let i = 0; i < pipeArray.length; i++) {             //Recorre todas las tuberias en el array
-        let pipe = pipeArray[i];        
+    for (let i = 0; i < pipeArray.length; i++) {             //Recorre todas las tuberias en el array
+        let pipe = pipeArray[i];
         pipe.x += velocityX;             //Mueve la tuberia hacia la izquierda usando la velocidad horizontal
 
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);   //Renderiza la tuberia sin escalar su altura
 
         //Condicional para sumar puntos si el pajaro paso la tuberia
-        if(!pipe.passed && bird.x > pipe.x + pipe.width) {      //Si el pajaro paso la tuberia y no se habia contado antes
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {      //Si el pajaro paso la tuberia y no se habia contado antes
             score += 0.5;          //Suma puntos 0.5 (par de tuberias = 1 punto)
             pipe.passed = true;     //Marca la tuberia como pasada
         }
 
         //Condicional para detectar colisiones entre el pajaro y las tuberias
-        if(detectCollision(bird, pipe)) {       //Si hubo una colision entre el pajaro y la tuberia
+        if (detectCollision(bird, pipe)) {       //Si hubo una colision entre el pajaro y la tuberia
             currentState = GAME_STATE.GAME_OVER;    //Activa el game over
         }
     }
 
     //Borra las tuberias que ya salieron del canvas
-    while(pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {        //Loop que borra la primera tuberia del array que este fuera del array
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {        //Loop que borra la primera tuberia del array que este fuera del array
         pipeArray.shift();
     }
 
-    //Renderiza el puntaje
-    context.fillStyle = "white";            //Color texto  
-    context.font = "45px sans-serif";       //Fuente y tamaño texto
-    context.textAlign = "left";             //Alineacion texto
-    context.fillText(score, 5, 45);         //Renderiza el puntaje en la esquina superior izquierda
+    // Renderiza el puntaje
+    context.font = "45px Flappy";       // Fuente y tamaño
+    context.textAlign = "left";         // Alineación
+    context.lineWidth = 6;              // Grosor del contorno
+    context.strokeStyle = "black";      // Color del contorno
+    context.fillStyle = "white";        // Color del texto
+    context.strokeText(score, 5, 45);   // Contorno
+    context.fillText(score, 5, 45);     // Relleno
+
 }
 
 //Funcion que renderiza el game over
-function renderGameOver() {    
-    if(backgroundImg.complete) {
+function renderGameOver() {
+    if (backgroundImg.complete) {
         context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);        //Renderiza el fondo
-    } 
-    if(gameOverImg.complete) {  //Condicional, si la imagen de gameover esta cargada
+    }
+    if (gameOverImg.complete) {  //Condicional, si la imagen de gameover esta cargada
         let imgWidth = 400;         //Ancho de la imagen gameover
         let imgHeight = 80;         //Alto de la imagen gameover
         let x = (boardWidth - imgWidth) / 2; //Coordenada x para centrar la imagen
@@ -234,15 +250,18 @@ function renderGameOver() {
 
         context.drawImage(gameOverImg, x, y, imgWidth, imgHeight);  //Renderiza la imagen de gameover centrada
 
-        //Formato del puntaje final
-        let scoreText = `Your score: ${Math.floor(score)}`;                           //Texto del puntaje final  
-        context.fillStyle = "white";                                                  //Color texto
-        context.font = "45px sans-serif";                                             //Fuente y tamaño texto
-        context.textAlign = "center";                                                 //Alineacion texto
-        context.fillText(scoreText, boardWidth / 2, y + imgHeight + 50);              //Renderiza el texto, abajo del gameover
+        // Formato del puntaje final
+        let scoreText = `Your score: ${Math.floor(score)}`;     // Texto del puntaje final  
+        context.font = "45px Flappy";                           // Fuente y tamaño texto
+        context.textAlign = "center";                           // Alineación
+        context.lineWidth = 6;                                  // Grosor del contorno
+        context.strokeStyle = "black";                          // Color del contorno
+        context.fillStyle = "white";                            // Color del texto
+        context.strokeText(scoreText, boardWidth / 2, y + imgHeight + 50);  // Contorno
+        context.fillText(scoreText, boardWidth / 2, y + imgHeight + 50);    // Relleno
 
         //Temporalmente bloquea el input para evitar un reinicio accidental
-        inputLocked = true;         
+        inputLocked = true;
         setTimeout(() => {
             inputLocked = false;
         }, 1000);
@@ -251,15 +270,15 @@ function renderGameOver() {
 
 //Funcion que maneja el input del usuario basado en los estados del juego (basicamente los controles)
 function handleKeyDown(e) {
-    if(inputLocked) return;         //Si el input esta bloqueado, return
+    if (inputLocked) return;         //Si el input esta bloqueado, return
 
-    if(e.code === "Space") {                                        //Condicional, si se presiono enter
-        if(currentState === GAME_STATE.MENU) {                         //Si estamos en el menu de inicio
+    if (e.code === "Space") {                                        //Condicional, si se presiono enter
+        if (currentState === GAME_STATE.MENU) {                         //Si estamos en el menu de inicio
             startGame();                                                  //Iniciar el juego
-        } else if(currentState === GAME_STATE.GAME_OVER) {              //Si estamos en gameover
+        } else if (currentState === GAME_STATE.GAME_OVER) {              //Si estamos en gameover
             resetGame();                                                  //Reiniciar el juego
             currentState = GAME_STATE.MENU;                                 //Volver al menu
-        } else if(currentState === GAME_STATE.PLAYING) {                //Si estamos jugando
+        } else if (currentState === GAME_STATE.PLAYING) {                //Si estamos jugando
             velocityY = -6;                                                //Hacer que el pajaro salte
         }
     }
@@ -269,12 +288,12 @@ function handleKeyDown(e) {
 function startGame() {
     currentState = GAME_STATE.PLAYING;      //Cambia el estado del juego, a jugando
     bird.y = birdY;         //Reinicia los valores del pajaro
-    velocityY = 0; 
+    velocityY = 0;
     pipeArray = [];         //Reinicia las tuberias
     score = 0;              //Reinicia el puntaje
 
     //Condicional
-    if(pipeIntervalId) {
+    if (pipeIntervalId) {
         clearInterval(pipeIntervalId);          //Borra la creacion de tuberias anterior
     }
 
